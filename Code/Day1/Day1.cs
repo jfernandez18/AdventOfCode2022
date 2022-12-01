2843d8b546805ed783652997e5c1a47b1051a5b9
Julio Fernández <xarambeloper@gmail.com>
2022-12-01 21:01:15 +0100

    Solution for the day1 Advent of Code challenge.

---
+++ Code/Day1/Day1.cs
@@ -0,0 +1,71 @@
+﻿
+namespace AdventOfCode2022.Code
+{
+    public class Day1
+    {
+        public static void CalculateCalories(string filePath)
+        {
+            // Lista de los 3 elfos con mas calorías.
+            List<int> moreCalorieElfs = new(3);
+            int calorieCounter = 0;
+            int min;
+
+            using StreamReader streamReader = new(filePath);
+
+            string? line;
+
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                if (!line.Equals(string.Empty))
+                {
+                    calorieCounter += int.Parse(line);
+                }
+                else
+                {
+                    // Obtenemos los 3 elfos con el mayor número de calorias
+                    if (moreCalorieElfs.Count < 3)
+                    {
+                        moreCalorieElfs.Add(calorieCounter);
+                    }
+                    else
+                    {
+                        min = moreCalorieElfs.Min();
+
+                        if (calorieCounter > min)
+                        {
+                            int index = moreCalorieElfs.IndexOf(min);
+                            moreCalorieElfs.RemoveAt(index);
+                            moreCalorieElfs.Add(calorieCounter);
+                        }                        
+                    }
+                    calorieCounter = 0;
+                }
+
+                if (streamReader.EndOfStream)
+                {
+                    // Obtenemos los 3 elfos con el mayor número de calorias
+                    if (moreCalorieElfs.Count < 3)
+                    {
+                        moreCalorieElfs.Add(calorieCounter);
+                    }
+                    else
+                    {
+                        min = moreCalorieElfs.Min();
+
+                        if (calorieCounter > min)
+                        {
+                            int index = moreCalorieElfs.IndexOf(min);
+                            moreCalorieElfs.RemoveAt(index);
+                            moreCalorieElfs.Add(calorieCounter);
+                        }
+                    }
+                }
+            }
+
+            moreCalorieElfs = moreCalorieElfs.OrderBy(x => x).ToList();
+
+            Console.WriteLine(String.Format("El número de calorías que tiene el elfo con más calorías es: {0}", moreCalorieElfs.Last()));
+            Console.WriteLine(String.Format("El número de calorías que tienen los tres elfos con más calorías es: {0}", moreCalorieElfs.Sum()));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCode2022.Code;
+
+Console.WriteLine("Advent of Code 2022\r\n");
+
+Console.WriteLine("Resultado del día 1\r\n");
+
+Day1.CalculateCalories(@"./data/day1/data.txt");
